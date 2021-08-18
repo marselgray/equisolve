e335bc8b0d63f2eddfb25b292318ee7e8c3f2a36
Marsel Gray <marselgray@gmail.com>
2021-08-18 16:23:20 -0400

    🚀 Update: modal styles

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.addEventListener('load', function() {
     loadData();
+    domLoader();
 });
 
+// dom loader of svg while data loads
+function domLoader(){
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('main').style.visibility = 'visible';
+}
 
-
+// loads data
 function loadData(){
 
     var url = `https://bensdemo.prod.equisolve-dev.com/api/v1/eq-test`;
@@ -14,15 +20,17 @@ function loadData(){
         .then(function(){
             // console.log(leadership);
 
-            leadership.map(function( item){
-                console.log(item)
+            leadership.map( (item) => {
+
+                // create unique id
                 let id = item.name
                 id = id.toLowerCase().replace(/\s/g, '');
-                console.log(id)
+                
+                // add data to cards
                 let bio = `
                     <div class="grid__card">
                         <input type="checkbox" id="${id}" class="input"/>
-                        <label for="${id}">
+                        <label class="label" for="${id}">
                             <img src="${item.photo_url}" alt="Photo of ${item.name}" class="bio__image">
                             <div class="bio">
                                 <p class="bio__name">${item.name}</p>
@@ -31,19 +39,26 @@ function loadData(){
                             </div>
                         </label>
                         <div class="modal">
-                            <p class="modal__name">${item.name}</p>
-                            <p class="modal__title">${item.title}</p>
-                            <div class="modal__box">
-                                ${item.bio}
-                                <img src="${item.photo_url}" alt="Photo of ${item.name}" class="modal__image">
+                            <div class="modal__container">
+                                <label for="${id}" class="modal__close" aria-label="close modal">X</label>
+                                <p class="modal__name">${item.name}</p>
+                                <p class="modal__title">${item.title}</p>
+                                <div class="modal__box">
+                                    ${item.bio}
+                                    <img src="${item.photo_url}" alt="Photo of ${item.name}" class="modal__image">
+                                </div>
                             </div>
                         </div>
                     </div>
                 `;
+
+                // add items to grid
                 document.getElementById('leadership').insertAdjacentHTML('beforeend', bio);
             });
             
 
         })        
 }
+
+
 
